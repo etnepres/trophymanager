c7b6c2cee7ee7471a99ab5d4159356c27d35618e
João Manuel Ferreira Fernandes <etnepres@gmail.com>
2016-11-05 17:12:09 +0000

    Update tm_super_club_squad.user.js

--- tm_super_club_squad.user.js
+++ tm_super_club_squad.user.js
@@ -14,71 +14,68 @@
 // @grant			none
 // ==/UserScript==
 
+//Functions needed
 
-
-
-    //Functions needed
-
-    function collect() {
-        var ret = {};
-        var len = arguments.length;
-        for (var i=0; i<len; i++) {
-            for (p in arguments[i]) {
-                if (arguments[i].hasOwnProperty(p)) {
-                    ret[p] = arguments[i][p];
-                }
+function collect() {
+    var ret = {};
+    var len = arguments.length;
+    for (var i=0; i<len; i++) {
+        for (var p in arguments[i]) {
+            if (arguments[i].hasOwnProperty(p)) {
+                ret[p] = arguments[i][p];
             }
         }
-        return ret;
+    }
+    return ret;
+}
+
+//Real Code
+
+var new_player_array = [];
+var team_b_id = "";
+
+$.ajaxSetup({async: false});
+
+$.post("/ajax/players_get_select.ajax.php",{"type":"change","club_id":SUBPAGE},function(data){
+
+    data = JSON.parse(data);
+    new_player_array = data.post;
+
+});
+
+$("#player_table tr:eq(0)").append('<td>ASI</td>');
+$("#player_table tr > .text_fade > div").not(".text_fade_overlay").find("a[player_link]").each(function(){
+
+    player_link = $(this).attr("player_link");
+
+    if(new_player_array[player_link] == null && team_b_id === ""){
+
+        console.log("finding team b id");
+
+        $.post("http://trophymanager.com/ajax/players_get_info.ajax.php",{"player_id":player_link, "type":"history","show_non_pro_graphs":false},function(data){
+
+            data = JSON.parse(data);
+            team_b_id = data.table.nat[0].klub_id;
+
+        });
+
+        $.post("/ajax/players_get_select.ajax.php",{"type":"change","club_id":team_b_id},function(data){
+
+            data = JSON.parse(data);
+            new_player_array = collect(new_player_array, data.post);
+
+        });
+
     }
 
-    //Real Code
+    current_player_info = new_player_array[player_link];
 
-    var new_player_array = [];
-    var team_b_id = "";
+    //console.log(current_player_info["asi"]);
 
-    $.ajaxSetup({async: false});
+    parent_select = $(this).parent().parent().parent();
 
-    $.post("/ajax/players_get_select.ajax.php",{"type":"change","club_id":SUBPAGE},function(data){
+    parent_select.append('<td>'+current_player_info.asi+'</td>');
 
-        data = JSON.parse(data);
-        new_player_array = data["post"];
+});
 
-    });
-
-    $("#player_table tr:eq(0)").append('<td>ASI</td>');
-    $("#player_table tr > .text_fade > div").not(".text_fade_overlay").find("a[player_link]").each(function(){
-
-        player_link = $(this).attr("player_link");
-
-        if(new_player_array[player_link] == null && team_b_id == ""){
-
-            console.log("finding team b id");
-
-            $.post("http://trophymanager.com/ajax/players_get_info.ajax.php",{"player_id":player_link, "type":"history","show_non_pro_graphs":false},function(data){
-
-                data = JSON.parse(data);
-                team_b_id = data["table"]["nat"][0]["klub_id"];
-
-            });
-
-            $.post("/ajax/players_get_select.ajax.php",{"type":"change","club_id":team_b_id},function(data){
-
-                data = JSON.parse(data);
-                new_player_array = collect(new_player_array, data["post"]);
-
-            });
-
-        }
-
-        current_player_info = new_player_array[player_link];
-
-        //console.log(current_player_info["asi"]);
-
-        parent_select = $(this).parent().parent().parent();
-
-        parent_select.append('<td>'+current_player_info["asi"]+'</td>');
-
-    });
-
-    $.ajaxSetup({async: true});
+$.ajaxSetup({async: true});
